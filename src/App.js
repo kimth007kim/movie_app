8b736d5dc16f8f53816dea670fc3241511fad08c
kimth007kim <kimth007kim@naver.com>
2020-01-19 23:01:27 +0900

    #3.0 Class Components and State 완료

--- src/App.js
+++ src/App.js
@@ -4,9 +4,21 @@ import PropTypes from "prop-types";
 class App extends React.Component {
   state = {
     count: 0
+  }; //바뀔숫자를 데이터에다가 넣음
+  add = () => {
+    console.log("add");
+  };
+  minus = () => {
+    console.log("minus");
   };
   render() {
-    return <h1>The number is: {this.state.count}</h1>;
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
   }
 }
 
