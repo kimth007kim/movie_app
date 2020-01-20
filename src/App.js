be194284daf240e4fc426aec4b189135a1cb4bed
kimth007kim <kimth007kim@naver.com>
2020-01-20 23:22:59 +0900

    #3.3 Planning the Movie Component 진행중

--- src/App.js
+++ src/App.js
@@ -3,22 +3,11 @@ import PropTypes from "prop-types";
 
 class App extends React.Component {
   state = {
-    count: 0
-  }; //바뀔숫자를 데이터에다가 넣음
-  add = () => {
-    this.setState(current => ({ count: this.state.count + 1 }));
-  };
-  minus = () => {
-    this.setState(current => ({ count: this.state.count - 1 }));
+    isLoading: true
   };
   render() {
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{this.state.isLoading ? "Loading..." : "We are ready"}</div>;
   }
 }
 
