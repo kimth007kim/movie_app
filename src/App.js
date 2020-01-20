98398bac883eb618d009ff1cede0db6de7305174
kimth007kim <kimth007kim@naver.com>
2020-01-20 23:30:28 +0900

    #3.3 Planning the Moive Component 완료

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import PropTypes from "prop-types";
 
 class App extends React.Component {
   state = {
-    isLoading: true
+    isLoading: true,
+    movies: []
   };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
+  }
   render() {
     const { isLoading } = this.state;
     return <div>{this.state.isLoading ? "Loading..." : "We are ready"}</div>;
