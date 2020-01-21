80a542f09813ac34dcc67333b68b340cfefd3ab5
kimth007kim <kimth007kim@naver.com>
2020-01-21 23:40:13 +0900

    #4.0 Fetching Movies from API 완료

--- src/App.js
+++ src/App.js
@@ -6,14 +6,15 @@ class App extends React.Component {
     isLoading: true,
     movies: []
   };
-  componentDidMount() {
-    axios.get("https://yts-proxy.now.sh/list_movies.json");
+  getMovies = async () => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+  };
+  async componentDidMount() {
+    this.getMovies();
   }
   render() {
     const { isLoading } = this.state;
-    return <div>{this.state.isLoading ? "Loading..." : "We are ready"}</div>;
-
-    //5분36초까지 들음
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
   }
 }
 
