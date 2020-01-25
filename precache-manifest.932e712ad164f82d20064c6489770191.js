c70a63fbe993508a328497b5edc4d3dbf6ba6f3c
kimth007kim <kimth007kim@naver.com>
2020-01-25 20:59:30 +0900

    Updates

---
+++ precache-manifest.932e712ad164f82d20064c6489770191.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "700213f7af43599c42b49e869f36e422",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "0296b6a74c5d2d616188",
+    "url": "/movie_app/static/css/main.6dbddcf4.chunk.css"
+  },
+  {
+    "revision": "f61bdc7f7bc08ddc47c9",
+    "url": "/movie_app/static/js/2.6efedadd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app/static/js/2.6efedadd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0296b6a74c5d2d616188",
+    "url": "/movie_app/static/js/main.6f982746.chunk.js"
+  },
+  {
+    "revision": "ca4b155c3570fabe1dd4",
+    "url": "/movie_app/static/js/runtime-main.98d66c69.js"
+  }
+]);
