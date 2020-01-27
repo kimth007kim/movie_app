959cddcf513aec479dd84f97b19423545ee3c358
kimth007kim <kimth007kim@naver.com>
2020-01-28 01:51:47 +0900

    CSS 기초 틀잡기

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,19 +4,18 @@ import "./Movie.css";
 
 function Movie({ year, title, summary, poster, genres }) {
   return (
-    <div className="movies__movie">
+    <div className="movie">
       <img src={poster} alt={title} title={title} />
       <div className="movie__data">
         <h3 className="movie__title">{title}</h3>
         <h5 className="movie__year">{year}</h5>
-        <ul className="genres">
+        <ul className="movie__genres">
           {genres.map((genre, index) => (
             <li key={index} className="genres__genre">
-              {index}
               {genre}
             </li>
           ))}
-          <p className="movie__summary">{summary.slice(0, 140)}</p>
+          <p className="movie__summary">{summary.slice(0, 180)}...</p>
         </ul>
       </div>
     </div>
