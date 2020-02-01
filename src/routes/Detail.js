46d56a340bd1c52a2fd1c649a7188c643e6d24d3
kimth007kim <kimth007kim@naver.com>
2020-02-01 23:19:48 +0900

    React강의 듣기 끝

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -2,12 +2,19 @@ import React from "react";
 
 class Detail extends React.Component {
   componentDidMount() {
-    const { location } = this.props;
-    console.log(location.state);
+    console.log(this.props);
+    const { location, history } = this.props;
+    if (location.state === undefined) {
+      history.push("/");
+    }
   }
   render() {
-    return <span>Hello</span>;
+    const { location } = this.props;
+    if (location.state) {
+      return <span>{location.state.title}</span>;
+    } else {
+      return null;
+    }
   }
 }
-
 export default Detail;
